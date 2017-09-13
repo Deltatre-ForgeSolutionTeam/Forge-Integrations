0e9092369296ecfb4c2026f95ced7aacb321c252
Mario Castellani <Mario.Castellani@deltatre.com>
2017-09-13 17:03:10 +0200

    Event recurrence update

--- polymer-webcomponents/event-recurrence/event-recurrence.js
+++ polymer-webcomponents/event-recurrence/event-recurrence.js
@@ -1,10 +1,20 @@
 (function(){
 
-    var _recurrenceSwitchEnabled = false;
+    var _recurrenceEnabled = false;
 
     function Recurrence(){
         this.period = null;
         this.days = [];
+    }
+
+    function RecurrenceTypeList(){
+        this.recurrenceTypeList = [];
+
+        this.recurrenceTypeList.push({ "value": "no-recurrence", "label": "no recurrence" });
+        this.recurrenceTypeList.push({ "value": "every-week", "label": "Every week" });
+        this.recurrenceTypeList.push({ "value": "every-2-weeks", "label": "Every 2 weeks" });
+        this.recurrenceTypeList.push({ "value": "every-3-weeks", "label": "Every 3 weeks" });
+        this.recurrenceTypeList.push({ "value": "every-4-weeks", "label": "Every 4 weeks" });
     }
 
     Polymer({
@@ -14,6 +24,10 @@
                 type: Object,
                 value: new Recurrence(),
                 observer: '_valueChanged'
+            },
+            recurrenceTypes:{
+                type: Object,
+                value: new RecurrenceTypeList()
             }
         },
 
@@ -25,17 +39,37 @@
             if (!newValue) this.value = new Recurrence();
         },
 
-        _recurrenceSwitchChanged: function (e){
-            //check se value è valorizzato e metterlo a true
-            
-            if(e.target.checked){
-                this._recurrenceSwitchEnabled = true;
-            }else{
-                this._recurrenceSwitchEnabled = false;
+        _recurrenceTypeSelected: function (e) {
+            var recurrenceType = this.value.period;
+            if(recurrenceType != "no-recurrence")
+            {
+                this._recurrenceEnabled = true;
             }
-                //svuotare oggetto prima di lanciare l'evento
+            else
+            {
+                this._recurrenceEnabled = false;
+            }
+            this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
+            console.log(recurrenceType);
+        },
 
-            console.log("change");
+        _dayCheckChanged: function(e){
+            var day = e.target.value;
+            if(day != null)
+            {
+                if(e.target.checked)
+                {
+                    this.value.days.push(day);
+                }
+                else
+                {
+                    var index = this.value.days.indexOf(day);
+                    this.value.days.splice(index, 1);
+                }
+                this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
+            }
+            console.log(day);
+            console.log(e);
         },
 
         _triggerValueChanged: function () {
