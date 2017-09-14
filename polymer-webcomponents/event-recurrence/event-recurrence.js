74482eec9d86266b9f68afbfc4eb6f1d9a238380
Mario Castellani <Mario.Castellani@deltatre.com>
2017-09-14 15:09:06 +0200

    Event recurrence component completion

--- polymer-webcomponents/event-recurrence/event-recurrence.js
+++ polymer-webcomponents/event-recurrence/event-recurrence.js
@@ -1,13 +1,14 @@
-(function(){
+(function () {
 
     var _recurrenceEnabled = false;
+    var _checkboxInitlist = [];
 
-    function Recurrence(){
+    function Recurrence() {
         this.period = null;
         this.days = [];
     }
 
-    function RecurrenceTypeList(){
+    function RecurrenceTypeList() {
         this.recurrenceTypeList = [];
 
         this.recurrenceTypeList.push({ "value": "no-recurrence", "label": "no recurrence" });
@@ -25,7 +26,7 @@
                 value: new Recurrence(),
                 observer: '_valueChanged'
             },
-            recurrenceTypes:{
+            recurrenceTypes: {
                 type: Object,
                 value: new RecurrenceTypeList()
             }
@@ -36,38 +37,55 @@
         },
 
         _valueChanged: function (newValue, oldValue) {
-            if (!newValue) this.value = new Recurrence();
+            if (!newValue) {
+                this.value = new Recurrence();
+            }
+            if (typeof newValue === "object" && typeof oldValue === "string") {
+                var recurrenceType = this.value.period;
+                if (recurrenceType != "no-recurrence") {
+                    this._recurrenceEnabled = true;
+                }
+                else {
+                    this._recurrenceEnabled = false;
+                }
+            }
         },
 
         _recurrenceTypeSelected: function (e) {
-            var recurrenceType = this.value.period;
-            if(recurrenceType != "no-recurrence")
-            {
+            var recurrenceType = e.target.recurrenceTypeValue;
+            if (recurrenceType != "no-recurrence") {
                 this._recurrenceEnabled = true;
             }
-            else
-            {
+            else {
                 this._recurrenceEnabled = false;
+                for(var d = 0; d < this.value.days.length; d++)
+                {
+                    var chkBox = this.$$('#' + this.value.days[d]);
+                    chkBox.checked = false;
+                }
+                this.value.days = [];
             }
             this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
-            console.log(recurrenceType);
         },
 
-        _dayCheckChanged: function(e){
+        _dayCheckChanged: function (e) {
             var day = e.target.value;
-            if(day != null)
-            {
-                if(e.target.checked)
-                {
+            if (day != null) {
+                if (e.target.checked) {
                     this.value.days.push(day);
                 }
-                else
-                {
+                else {
                     var index = this.value.days.indexOf(day);
                     this.value.days.splice(index, 1);
                 }
                 this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
+            } else {
+                var id = e.target.id;
+                if (this.value.days.includes(id)) {
+                    e.target.checked = true;
+                }
             }
+            console.log("_dayCheckChanged ");
             console.log(day);
             console.log(e);
         },
