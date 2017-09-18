0317da0e9a4eebc520fb4aa051823d8f55a84a92
Mario Castellani <Mario.Castellani@deltatre.com>
2017-09-18 14:29:25 +0200

    Story Part Quote update with 2 input fields

--- polymer-webcomponents/story-part-quote/story-part-quote.js
+++ polymer-webcomponents/story-part-quote/story-part-quote.js
@@ -1,11 +1,16 @@
 (function () {
 
+    function QuoteData() {
+        this.quote = null;
+        this.author = null;
+    }
+
     Polymer({
         is: "story-part-quote",
         properties: {
             value: {
                 type: Object,
-                value: {},
+                value: new QuoteData(),
                 observer: '_valueChanged'
             }
         },
@@ -16,8 +21,16 @@
 
         _valueChanged: function (newValue, oldValue) {
             if (!newValue) {
-                this.value = {};
+                this.value = new QuoteData();
             }
+        },
+
+        _quoteChanged: function(e){
+            Console.log("quoteChanged: " + e);
+        },
+
+        _authorChanged: function(e){
+            Console.log("authorChanged: " + e);
         }
     });
 })();
