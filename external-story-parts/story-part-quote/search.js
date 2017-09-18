1867017f5af761cf7c26868dd3518cf99c1a7fcc
Mario Castellani <Mario.Castellani@deltatre.com>
2017-09-18 17:23:24 +0200

    Story Part quote - api render completion

--- external-story-parts/story-part-quote/search.js
+++ external-story-parts/story-part-quote/search.js
@@ -10,7 +10,16 @@ app.get('/story-part-quote/search', function (req, res) {
 app.post('/story-part-quote/search', function (req, res) {
 
 	var quote = req.body.quoteValue;
-	var result = "result";
+	var result = '<pre>'
+	+ '<div style="overflow: hidden; position: relative;">'
+		+ '<div style="float:left; width:10%; text-align: left; -webkit-transform: rotateY(180deg); moz-transform: rotateY(180deg); -o-transform: rotateY(180deg); -ms-transform: rotateY(180deg);"><i class="material-icons" style="color:gray; font-size:60px">format_quote</i></div>'
+		+ '<div style="float:left; width:80%; padding:20px; min-height:130px; border:dotted 1px #999; white-space: pre-wrap; position: relative;">'
+			+ '<div>' + quote.quote + '</div>'
+			+ '<div style="padding-top:10px; padding-bottom:10px; text-align:right; font-style:italic; font-weight:bold; font-size:16px;">' + quote.author + '</div>'
+		+ '</div>'
+		+ '<div style="float:left; width:10%; position: absolute; bottom:0px; right:0px;"><i class="material-icons" style="color:gray; font-size:60px">format_quote</i></div>'
+	+ '</div>'
+	+'</pre>';
 	var quoteData = { "quote": quote.quote, "author": quote.author };
 
 
