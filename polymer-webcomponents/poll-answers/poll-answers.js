f574c4529ab6ae0cce96b60a50c19d369fd80b1c
Andrea Dutto <Andrea.dutto@deltatre.it>
2017-09-20 19:50:04 +0200

    Add component poll-answers

---
+++ polymer-webcomponents/poll-answers/poll-answers.js
@@ -0,0 +1,68 @@
+(function () {
+
+    const answerType = "text";
+
+    function Answer(answerType){
+        this.type = answerType;
+        this.content = null;
+    };
+
+    function  AnswerList(){
+        this.answers = [];
+    }
+
+    Polymer({
+        is: "poll-answers",
+        behaviors: [
+            ForgeWebComponents.Behaviors.ForgeLocalizeBehavior
+        ],
+        properties: {
+            value: {
+                type: Object,
+                value: new AnswerList(),
+                observer: '_valueChanged'
+            }
+
+        },
+
+        ready: function () {
+            this._linkToContentType = false;
+            this._externalLinkType = false;
+        },
+
+        _valueChanged: function (newValue, oldValue) {
+            if (!newValue) this.value =  new AnswerList();
+        },
+
+        _addAnswer: function(e){
+            var path = "value.answers";
+            this.push(path, new Answer(answerType));
+            
+            this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
+        },
+
+        _onAnswerInput: function(){
+            this.debounce('triggerOnValueChanged', this._triggerValueChanged, 500);
+        },
+
+        _onAnswerChange: function(){
+            this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
+        },
+
+        _deleteAnswer: function(e){
+            var path = "value.answers";
+            this.splice(path, e.model.answerIndex, 1);
+            this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
+
+        },
+
+        _triggerValueChanged: function () {
+            this.fire('valueChanged', this.value);
+        }
+
+
+    });
+
+
+
+})();
