cade108c1b202c32319ba6b0b07e78f2f9e335ad
Andrea Dutto <Andrea.dutto@deltatre.it>
2017-09-21 20:52:07 +0200

    Add Css and Loading progress bar

--- polymer-webcomponents/poll-result/poll-result.js
+++ polymer-webcomponents/poll-result/poll-result.js
@@ -1,6 +1,20 @@
 (function () {
 
-    var oldPollEntityId = null;
+    var api = ForgeWebComponents.Api;
+
+
+    function PollResults() {
+        this.pollQuestionText = null;
+        this.answers = [];
+
+    }
+
+    function PollAnswer(EntityData, ShieldData) {
+        this.answerText = EntityData.content;
+        this.id = EntityData.id;
+        this.numberVote = ShieldData.numberVote;
+        this.votePercentage = Math.round(parseFloat(ShieldData.votePercentage) * 100);
+    }
 
     Polymer({
         is: "poll-result",
@@ -10,41 +24,115 @@
         properties: {
             value: {
                 type: Object,
-                value: { },
+                value: new PollResults(),
                 observer: '_valueChanged'
             },
-
+            entity: {
+                type: Object,
+                observer: '_entityChanged'
+            },
             moduleEnabled: {
                 type: Boolean,
                 value: false
             },
-            pollResult: {
-                type: Array,
-                value: [ ]
+            _pollData: {
+                type: Object,
+                value: {}
             }
         },
 
         ready: function () {
-            this._pollResultLoad();
+            this._moduleEnabled = false;
+            this._pollData = {};
+
+            console.log(this.entity);
         },
+
+
+        _getData: function (entityId) {
+
+            var url = "deltatre.forge.wcm/api/customEntities/poll/working/culture/en-us/entityid/" + entityId;
+
+            var self = this;
+            self._hideLoading = false;
+
+            api.raw(url).then(function (result) {
+                self._hideLoading = true;
+                self._pollData = result;//enrichResultItems(result);
+                self._pollResultLoad();
+            }, function () {
+                self._hideLoading = true;
+                self._pollData = {};
+                console.error(arguments);
+            });
+
+        },
+
 
         _valueChanged: function (newValue, oldValue) {
-            if (!newValue) this.value =  [];
+            if (!newValue) this.value = new PollResults();
         },
 
-        _pollResultLoad: function() {
+        _entityChanged: function (newValue, oldValue) {
+            if (!newValue) {
+                this.entity = {};
+            } else {
+                if (this.entity.entityId) {
+                    this._getData(this.entity.entityId);
+                }
+            }
+        },
+
+
+        _pollResultLoad: function () {
             this.$.requestPollResult.generateRequest();
         },
 
-        _pollResultResponse: function(data) {
-            this.pollResult = data.detail.response.pollResult;
+        _pollResultResponse: function (data) {
+            this.value.pollQuestionText = this._pollData.Title;
 
-            if (this.pollResult.length > 0)
-                this.moduleEnabled = true;
-            else
-                this.moduleEnabled = false;
-        }
+            var pollAnswerList = this._pollData.ExtendedFields.answers;
+            var pollShieldAnswerList = data.detail.response.pollResult;
+
+            for (var i = 0; i < pollAnswerList.length; i++) {
+                var answer = pollAnswerList[i];
+
+                var answerVote = pollShieldAnswerList.find(function (s) {
+                    if (s.answerKey === answer.id) {
+                        return s;
+                    }
+                });
+
+                var path = "value.answers";
+
+                if (answerVote) {
+                    this.push(path, new PollAnswer(answer, answerVote))
+                } else {
+                    var emptyShieldData = {
+                        numberVote: "0",
+                        votePercentage: "0"
+                    };
+
+                    this.push(path, new PollAnswer(answer, emptyShieldData))
+                }
+            }
+
+            this._moduleEnabled = true;
+
+            console.log(this.value);
+
+
+        },
+
+        _refreshData: function () {
+            this._moduleEnabled = false;
+            this._getData(this.entity.entityId);
+        },
+
+
+
     });
+
 
 
 })();
