c71ee81dfba929c75ba230bf979d50a3d2f9fe4d
lorisfasolo-deltatre <31985123+lorisfasolo-deltatre@users.noreply.github.com>
2017-10-06 10:04:22 +0200

    poll-answers: fixed the bug when add 10 answers, re-open the poll and the "add" button is shown

--- polymer-webcomponents/poll-answers/poll-answers.js
+++ polymer-webcomponents/poll-answers/poll-answers.js
@@ -1,110 +1,113 @@
 (function () {
 
-        const answerType = "text";
-       
-        function Answer(answerType){
-            this.type = answerType;
-            this.content = null;
-            this.id = uuid.v4()
-        };
-    
-        Polymer({
-            is: "poll-answers",
-            behaviors: [
-                ForgeWebComponents.Behaviors.ForgeLocalizeBehavior
-            ],
-            properties: {
-                value: {
-                    type: Array,
-                    value: [],
-                    observer: '_valueChanged'
-                }  
-            },
-          
-            ready: function () {
-                this._linkToContentType = false;
-                this._externalLinkType = false;
-             
-            },
-    
-            _valueChanged: function (newValue, oldValue) {
-                if (!newValue) this.value =  [];
-            },
-    
-            _addAnswer: function(e, answer){
+    const answerType = "text";
 
-                var path = "value";
-                var positionLength = this.value.length;
-                document.querySelector('paper-button').hidden = false;
+    function Answer(answerType) {
+        this.type = answerType;
+        this.content = null;
+        this.id = uuid.v4()
+    };
 
-                if(positionLength == 0){
-                    this.push(path, new Answer(answerType));
-                    this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);                  
-                }
-                else{
-                    if(positionLength <= 9)
-                    {
-                        if(Boolean(this.value[positionLength - 1].content)){       
-                            var lastPositionInsert = this.value[positionLength - 1];
-                            if(positionLength != 9){
-                                if(lastPositionInsert.content){
-                                    this.push(path, new Answer(answerType));
-                                    this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
-                                }else{
-                                    toastWarningPoll.open();
-                                    return;
-                                }   
-                            }else{
-
-                                this.push(path, new Answer(answerType));
-                                this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
-                                document.querySelector('paper-button').hidden = true;
-                            }
-                        }
-                        else{
-                            toastWarningPoll.open();
-                            return;
-                        }
-                    }else{
-                        //e.target.hidden = true;
-                        document.querySelector('paper-button').hidden = true;
-                    }
-                }       
+    Polymer({
+        is: "poll-answers",
+        behaviors: [
+            ForgeWebComponents.Behaviors.ForgeLocalizeBehavior
+        ],
+        properties: {
+            value: {
+                type: Array,
+                value: [],
+                observer: '_valueChanged'
             },
-    
-            _onAnswerInput: function(){
-                this.debounce('triggerOnValueChanged', this._triggerValueChanged, 500);
-            },
-    
-            _onAnswerChange: function(){
-                this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
-            },
-    
-            _deleteAnswer: function(e){
-                
-                var path = "value";
-                var _index = e.model.dataHost.answerIndex;
-                this.splice(path, _index, 1);
-                this.debounce('triggerOnValueChanged', this._triggerValueChanged, 0);
-                console.log(document.querySelector('paper-button'));
-                console.log(this.value.length);
-
-                if(this.value.length < 10){
-                    document.querySelector('paper-button').hidden = false;
-                }
-            },
-
-            _isTextAnswer: function(answer,indexAnswer){
-
-                if(indexAnswer >= 9)
-                {
-                    document.querySelector('paper-button').hidden = true;
-                }
-                return answer.type === "text" ? true : false;
-            },
-    
-            _triggerValueChanged: function () {   
-                this.fire('valueChanged', this.value);
+            _showAddButton: {
+                type: Boolean,
+                value: true
             }
-        });
-    })();
+        },
+
+        ready: function () {
+            this._linkToContentType = false;
+            this._externalLinkType = false;
+
+        },
+
+        _valueChanged: function (newValue, oldValue) {
+            if (!newValue) this.value = [];
+        },
+
+        _addAnswer: function (e, answer) {
+
+            var path = "value";
+            var positionLength = this.value.length;
+            this._showAddButton = true;
+
+            if (positionLength == 0) {
+                this.push(path, new Answer(answerType));
+                this._callValueChanged(0);
+            }
+            else {
+                if (positionLength <= 9) {
+                    if (Boolean(this.value[positionLength - 1].content)) {
+                        var lastPositionInsert = this.value[positionLength - 1];
+                        if (positionLength != 9) {
+                            if (lastPositionInsert.content) {
+                                this.push(path, new Answer(answerType));
+                                this._callValueChanged(0);
+                            } else {
+                                toastWarningPoll.open();
+                                return;
+                            }
+                        } else {
+
+                            this.push(path, new Answer(answerType));
+                            this._callValueChanged(0);
+                            this._showAddButton = false;
+                        }
+                    }
+                    else {
+                        toastWarningPoll.open();
+                        return;
+                    }
+                } else {
+                    this._showAddButton = false;
+                }
+            }
+        },
+
+        _onAnswerInput: function () {
+            this._callValueChanged(500);
+        },
+
+        _onAnswerChange: function () {
+            this._callValueChanged(0);
+        },
+
+        _deleteAnswer: function (e) {
+            var path = "value";
+            var _index = e.model.dataHost.answerIndex;
+
+            this.splice(path, _index, 1);
+            this._callValueChanged(0);
+
+            if (this.value.length < 10) {
+                this._showAddButton = true;
+            }
+        },
+
+        _isTextAnswer: function (answer, indexAnswer) {
+            if (indexAnswer >= 9) {
+                this._showAddButton = false;
+            }
+
+            return answer.type === "text" ? true : false;
+        },
+
+        _callValueChanged: function (mills) {
+            this.debounce('triggerOnValueChanged', this._triggerValueChanged, mills);
+        },
+
+        _triggerValueChanged: function () {
+            this.fire('valueChanged', this.value);
+        }
+    });
+})();
